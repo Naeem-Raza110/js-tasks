51a3b74e8c17750fdf3281c47a531c72ea09d2d0
Naeem-Raza110 <123@gmail.com>
2024-09-11 04:20:43 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// varibles
+
+
+
+
+
+// arthmartic opperators
+
+
+let Sudent_Data =5 ;
+let b =3 ;
+
+
+
+
+
+
+// add
+// sub  
+// multifiication
+//division
+// exponent
+// modules
+
+
+
+console.log("a%b" , a%b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
