ca3f449bc204da8fa021d6395eedbe57e5fbd286
Naeem-Raza110 <123@gmail.com>
2024-09-13 05:31:24 -0700

    change

--- app.js
+++ app.js
@@ -1,48 +1,209 @@
-let num = prompt("Enter a number:");
+//conditional  statemnts
+        // -> if statment  0dd/even
+        // else // false  statment ka leya istamal hota ha
+        // -> else if  statment -->
+        // switch statment --> excute karna ka leyu istamal hota ha
+        // tarnary oprators ---.compact
+        //a? b:c
+        // used to perform d/f action on the based of d/f condation //
+        //prompt
+        // alert
+ //if else
+//  let age= 17;
+//  if (age >= 18) {
+//    console.log( "you can cast the vote");//
+//  }else{
+//    console.log("you can not cost the vote ");}//
+//    let data = 15;
+//    if (data % 2 == 0){
+//      console.log("even");
+//    }else{
+//      console.log("odd");
+//    }
+//    let mode = "purple";
+//    let color;
+//    if (mode == "light"){
+//      color = "yellow";
+//      console.log("yellow ")
+//    }else if (mode == "red"){
+//        color = "red"
+//        console.log( color," red")
+//    }else if (mode == "green"){
+//      color = "red"
+//      console.log( color," green")
+//    }else {
+//      color = "white"
+//      console.log("white ")
+//    };
 
-if (num % 5 === 0){
-    console.log(num, " is multiple of 5");
+
+
+// let num = prompt("Enter a number:");
+
+// if (num % 5 === 0){
+//     console.log(num, " is multiple of 5");
     
-}else{
-    console.log(num, " is not multiple of 5");
-}
+// }else{
+//     console.log(num, " is not multiple of 5");
+// }
 
-let age= 20;
+// let age= 20;
 
-if (age >= 18) {
-    console.log( "you can cast vote" );
-}else{
-    console.log( "you cannot cast vote" );
+// if (age >= 18) {
+//     console.log( "you can cast vote" );
+// }else{
+//     console.log( "you cannot cast vote" );
     
-}
+// }
+
+// tarnary oprators ---.compact
+let num = prompt("Enter a number")
+let result = num % 5 == 0 ?  console.log(num, " is multiple of 5") :  console.log(num, " is not multiple of 5");
+
+
 
 //switch statment
-
-let day= 10;
-
+let day = 1;
 switch (day) {
-case 0:
-    console.log(monday);
-break;
-
-case 1:
-    console.log(monday);
-break;
-
-case 2:
-   console.log(monday);
-   break;   
-
+  case 1:
+    console.log("sunday");
+    break;
+  case 2:
+    console.log("monday");
+    break;
+  case 3:
+    console.log("tuesday");
+    break;
+  case 4:
+    console.log("wednesday");
+    break;
+  case 5:
+    console.log("thursday");
+    break;
+  case 6:
+    console.log("friday");
+    break;
+  case 7:
+    console.log("saturday");
+    break;
+  default:
+    console.log("invalid day");
 }
 
 
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="UTF-8" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <title>Document</title>
+//     <link rel="stylesheet" href="styles.css" />
+//   </head>
+//     <body class="light-mode">
+//       <button onclick="toggleMode()">Toggle Mode</button>
+//       <script src="script.js"></script>
+//   </body>
+// </html>
+// //css code
+// body.dark-mode {
+//     background-color: #333;
+//     color: #fff;
+//   }
+//   body.light-mode {
+//     background-color: #fff;
+//     color: #000;
+//   }
+// //js code.
+// // Data types
+// // number
+// //sting
+// //boolean
+// //undefined
+// //conditional  statemnts
+// // -> if statment  0dd/even
+// // else // false  statment ka leya istamal hota ha
+// // -> else if  statment -->
+// // switch statment --> excute karna ka leyu istamal hota ha //weekdaay/
+// // tarnary oprators ---.compact
+// //a? b:c
+// // used to perform d/f action on the based of d/f condation //
+// //prompt
+// // alert
+// //if else
+// // let age= 17;
+// // if (age >= 18) {
+// //   console.log( "you can cast the vote");//
+// // }else{
+// //   console.log("you can not cost the vote ");}//
+// //   let data = 15;
+// //   if (data % 2 == 0){
+// //     console.log("even");
+// //   }else{
+// //     console.log("odd");
+// //   }
+// let mode = "preple";
+// let color;
+// switch (mode) {
+//   case "yellow":
+//     color = "yellow";
+//     break;
+//   case "red":
+//     color = "red";
+//     break;
+//   case "green":
+//     color = "green";
+//     break;
+//   default:
+//     color = "white";
+// }
+// // alert(color);
+// //tarnary oprators
+// // let num = prompt("enter the number");
+// // let result = num % 5 == 0 ? "multiple of the five " : "oNOT a multiple of the five ";
+// // console.log(result);
+// // switch statment
+// let day = 0;
+// console.log("please enter a relevant day(1-7)");
+// switch (day) {
+//   case 0:
+//     //  alert("monday")
+//     break;
+//   case 1:
+//     console.log("tuesday");
+//     break;
+//   case 2:
+//     console.log("thursday");
+//     break;
+//   case 3:
+//     console.log("friday");
+//     break;
+//   case 4:
+//     console.log("saturday");
+//     break;
+//   case 5:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("invalid day");
+//     break;
+// }
+// function toggleMode() {
+//   if (document.body.classList.contains("dark-mode")) {
+//     document.body.classList.remove("dark-mode");
+//     document.body.classList.add("light-mode");
+//   } else {
+//     document.body.classList.add("dark-mode");
+//     document.body.classList.remove("light-mode");
+//   }
+// }
+// let num = prompt("enter the number");
+// let result =
+//   num % 5 == 0
+//     ? console.log("multiple of the five ")
+//     : console.log("oNOT a multiple of the five ");
 
 
-function toggleMode() {
- if 
-}
 
 
-
-
+//loops
 
